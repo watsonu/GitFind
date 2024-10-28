2235e38fa19efdf1996deaef8d96ea7c5593a1a1
Watson Breno <watsonbreno2014@gmail.com>
2024-10-28 16:42:44 -0300

    Falta aparecer o repositório

---
+++ src/components/ItemList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+
+function ItemList({title, description}) {
+  return (
+    <div className='item-list'>
+        <strong>{title}</strong>
+        <p>{description}</p>
+        <hr />
+    </div>
+  )
+}
+
+export default ItemList
